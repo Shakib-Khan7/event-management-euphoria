2dc550498d27f270d84804acafc495efcaf644c5
Shakib <shakib.k2000@gmail.com>
2024-06-12 21:50:50 +0600

    Ready for deployment

--- README.md
+++ README.md
@@ -6,3 +6,14 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+//Website Description
+** It is a Entertainment Event Management website where user can get the best service for any entertainment event they want to organize.The Euphoria Event Management handles all aspects of users desired event.
+The website provides best internet experience to users to book any upcoming events from their choice.
+
+*Functionalities* 
+-> User can login,register and sign out
+->User can view Upcoming events and Our Services
+->User can view event details
+->User can book an event
+->User can cancel booking
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -10,24 +10,27 @@ import AboutUsBriefly from '../../components/AboutUsBriefly';
 import Services from '../../components/Services';
 import { json, useLoaderData } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
+import Aos from 'aos';
+import 'aos/dist/aos.css'
 
 const Home = () => {
 
-    const [myServices,setMyServices] = useState({})
+    const [myServices, setMyServices] = useState({})
 
-    useEffect(()=>{
-        fetch('/public/services.json')
-        .then(res=>res.json())
-        .then(data=>setMyServices(data))
-    },[])
-    
-    
-    
+    useEffect(() => {
+        Aos.init()
+        fetch('/services.json')
+            .then(res => res.json())
+            .then(data => setMyServices(data))
+    }, [])
+
+
+
 
     return (
         <div className=''>
-            
-            <div className="carousel w-full relative ">
+
+            <div className="carousel w-full relative " >
                 <div id="slide1" className="carousel-item relative w-full">
                     <div className='banner text-white' style={{ backgroundImage: `url(${banner})` }}>
                         <Navbar></Navbar>
@@ -54,7 +57,7 @@ const Home = () => {
                 </div>
             </div>
 
-            <div>
+            <div data-aos="fade-in">
                 <center>
                     <h2 className='mb-10 mt-20 text-xl tracking-[8px]'>UPCOMING EVENTS</h2>
 
@@ -63,7 +66,7 @@ const Home = () => {
                 </center>
             </div>
 
-            <div className='w-full bg-slate-100 grid grid-cols-1 lg:grid-cols-2 py-32'>
+            <div className='w-full bg-slate-100 grid grid-cols-1 lg:grid-cols-2 py-32' >
                 <div className='text-center'>
                     <h1 className='tracking-widest text-2xl font-poppins mb-9 text-slate-500'>WE ARE EUPHORIA</h1>
                     <h1 className=' text-3xl lg:text-7xl text-slate-900 tracking-wider font-poppins'>
@@ -71,17 +74,21 @@ const Home = () => {
                         Entertainment <br /> Event Management
                     </h1>
                     <p className='px-20 mt-10 text-slate-600 text-xl mb-5'>
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit numquam rem illum repudiandae vero deleniti ducimus possimus unde voluptatibus assumenda iure cumque, eaque ex excepturi ipsum doloremque non quasi molestias.
+                        Welcome to Euphoria Event Management, where we provide unparalleled services for organizing your entertainment events. Our expert team handles every aspect of your event, ensuring a seamless and unforgettable experience. Whether you're planning a concert, a private party, or a corporate gathering, we tailor our services to meet your specific needs.
+
+                        Our website offers a top-tier online experience, allowing you to effortlessly book upcoming events of your choice. With Euphoria Event Management, you're guaranteed the best service and a flawless execution of your event from start to finish.
                     </p>
                 </div>
-                <AboutUsBriefly></AboutUsBriefly>
+                <div data-aos="zoom-in" >
+                <AboutUsBriefly ></AboutUsBriefly>
+                </div>
             </div>
 
-            <div>
+            <div >
                 <h2 className='text-center text-3xl mb-5 mt-20 tracking-widest text-slate-500'>Our Services</h2>
                 <h2 className='text-5xl mb-20 text-center'>Euphoria <span className='font-bold'>Expertise</span></h2>
 
-                <div className='grid lg:grid-cols-2 lg:w-8/12 mx-auto h-full gap-10'>
+                <div className='grid lg:grid-cols-2 lg:w-8/12 mx-auto h-full gap-10 ' data-aos="fade-left" >
                     {myServices && myServices.events ? (
                         myServices.events.map(myService => (
                             <Services key={myService.id} service={myService}></Services>
